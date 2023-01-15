5b53bb58afdae431aef9c46eea163f4c6b0efc6e
sn <shurick2211@gmail.com>
2023-01-15 20:57:00 +0200

    forAll + data

--- src/main/java/com/nimko/org/websocketexample/controllers/ChatController.java
+++ src/main/java/com/nimko/org/websocketexample/controllers/ChatController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ChatController {
 
-    @GetMapping("/chat")
+    @GetMapping("/")
     public String index() { return "index"; }
 }
--- src/main/resources/static/myscript.js
+++ src/main/resources/static/myscript.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://localhost:8080/mess");
+const ws = new WebSocket("ws://localhost:5000/mess");
 
 ws.onopen = function (){
 
@@ -16,18 +16,18 @@ function action(message) {
     const newP = document.createElement("p");
     const newI = document.createElement("i");
     const strMess = message.toString()
-    const dblDot = strMess.indexOf(':')
+    const dblDot = strMess.indexOf('/:')
     const name = strMess.substring(0, dblDot);
     const mess = strMess.substring(dblDot);
-    const data = new Date();
-    newI.appendChild(document.createTextNode(name+' / ' + data.toLocaleString() + " "));
+    newI.appendChild(document.createTextNode(name+ " "));
     newP.appendChild(newI);
     newP.appendChild(document.createTextNode(mess));
     output.appendChild(newP);
 }
 
 function sendMess(){
+    const data = new Date();
     let name = document.getElementById("name").value;
     let message = document.getElementById("mess").value;
-    ws.send( name + ': ' + message);
+    ws.send( name + ' / ' + data.toLocaleString() + ' /: ' + message);
 }
